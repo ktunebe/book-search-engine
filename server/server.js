3eabc0a738ec2ef212eec1967d21ec35b3f3133c
Kent Tuneberg <ktunebe@gmail.com>
2024-08-03 13:33:33 -0500

    updated auth and apollo server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+db.once('open', async () => {
 
-app.use(routes);
+  await apolloServer.start()
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.use('/graphql', expressMiddleware(apolloServer))
+
+  app.listen(PORT, () => {
+    console.log(`Express server running on port ${PORT}!`);
+    console.log(`GraphQL available at http://localhost:${PORT}/graphql`)
+  });
 });
+
